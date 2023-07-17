1e522a1aa92208ce655083ac26007d6d1aa28d4f
NoobiesDoobies <94593468+NoobiesDoobies@users.noreply.github.com>
2023-07-17 13:23:48 +0700

    smoothstep

--- CDPR/02_IK.ino
+++ CDPR/02_IK.ino
@@ -1,3 +1,13 @@
+struct Point {
+  float x;
+  float y;
+  float z;
+};
+const int n = 100;
+
+struct Point points[n];
+float angleBetweenPoints[n-2];
+
 const float stepsPerRevolution = 1600.0 ;
 const float kelilingExtruder = PI*28.5 ; // mm
 const float stepAmount = kelilingExtruder/stepsPerRevolution; // mm/step
@@ -10,6 +20,7 @@ const float effectorLength = 70.0; // mm
 const float effectorWidth = 70.0;  // mm
 const float effectorHeight = 40.0; // mm
 const float vertexRadius = 60.0;   // mm
+const float zBias = 170.0;
 
 
 // Setup constants
@@ -21,27 +32,80 @@ int step1;
 int step2;
 int step3;
 int step4;
-float l1Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(170.0, 2.0));
-float l2Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(170.0, 2.0));
-float l3Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(170.0, 2.0));
-float l4Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(170.0, 2.0));
+float l1Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(zBias, 2.0));
+float l2Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(zBias, 2.0));
+float l3Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(zBias, 2.0));
+float l4Prev = sqrt(pow(sqrt(pow(boxLength/2.0 - effectorLength/2.0, 2.0) + pow(boxWidth/2.0 - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0) + pow(zBias, 2.0));
 
 
+float currentPosition[3] = {boxLength/2.0, boxWidth/2.0, zBias};
 
-// float X[4] = {150.0 + boxLength/2.0, -150.0 + boxLength/2.0, -150.0 + boxLength/2.0, 150.0 + boxLength/2.0};
-// float Y[4] = {150.0 + boxWidth/2.0,  150.0 + boxWidth/2.0,  -150.0 + boxWidth/2.0,  -150.0 + boxWidth/2.0};
-// float Z[4] = {0};
+float getAngleBetweenPoints(struct Point point1, struct Point point2, struct Point point3){
+    float a1 = point3.x - point2.x;
+    float a2 = point3.y - point2.y;
+    float a3 = point3.z - point2.z;
+    float b1 = point1.x - point2.x;
+    float b2 = point1.y - point2.y;
+    float b3 = point1.z - point2.z;
 
-const int n = 20;
-float X[n] = {0};
-float Y[n] = {0};
-float Z[n] = {0};
+    float dot = a1*b1 + a2*b2 + a3*b3;
+    float cross = sqrt(pow(a1*b2 - b1*a2, 2.0) + pow(a2*b3 - a3*b2, 2.0) + pow(a3*b1 - a1*b3, 2.0));
+    float theta = PI - fabs(atan2(dot, cross));
+  return theta;
+}
 
-void initXYZ(){
+void calculateAngleBetweenPoints(){
+  angleBetweenPoints[0] = M_PI/2;
+  angleBetweenPoints[n-1] = M_PI/2;
+
+  for (int i = 1; i < n - 1; i++) {
+    angleBetweenPoints[i] = getAngleBetweenPoints(points[i-1], points[i], points[i+1]);
+  }
+}
+
+void initFirstPoint(){
+  points[0].x = 0.0;
+  points[0].y = 0.0;
+  points[0].z = 0.0;
+}
+void initCirclePath(float R){
   for (int i = 0; i < n; i++) {
-    X[i] = 150.0*cosf((float) i/n*2*PI)/fmaxf(fabs(cosf((float) i/n*2*PI)), fabs(sinf((float) i/n*2*PI))) + boxLength/2.0;
-    Y[i] = 150.0*sinf((float) i/n*2*PI)/fmaxf(fabs(cosf((float) i/n*2*PI)), fabs(sinf((float) i/n*2*PI))) + boxWidth/2.0;
-    Z[i] = 170.0;
+    points[i].x = R*cosf(i/(float)n*2*PI) + boxLength/2.0;
+    points[i].y = R*sinf(i/(float)n*2*PI) + boxWidth/2.0;
+    points[i].z = zBias;
+  }
+}
+
+void initEllipsePath(float a, float b){
+  for (int i = 0; i < n; i++) {
+    float angle = i / static_cast<float>(n) * 2 * M_PI;
+    float x = a * cosf(angle) + boxLength/2.0;
+    float y = b * sinf(angle) + boxWidth/2.0;
+    points[i].x = x;
+    points[i].y = y;
+    points[i].z = zBias;
+  }
+}
+
+void initFlowerPath(float R, int petalCount){
+  for (int i = 0; i < n; i++) {
+    float angle = i / static_cast<float>(n) * 2 * M_PI;
+    float x = R * cosf(petalCount * angle) * cosf(petalCount * angle) + boxLength/2.0;
+    float y = R * sinf(angle) * cosf(petalCount * angle) + boxWidth/2.0;
+    points[i].x = x;
+    points[i].y = y;
+    points[i].z = zBias;
+  }
+}
+
+void initEightPath(float R) {
+  for (int i = 0; i < n; i++) {
+    float angle = i / static_cast<float>(n) * 2 * M_PI;
+    float x = R * 1.5 * cosf(angle) + boxLength / 2.0;
+    float y = R * sinf(2 * angle) / 1.5 + boxWidth / 2.0;
+    points[i].x = x;
+    points[i].y = y;
+    points[i].z = zBias;
   }
 }
 
@@ -54,18 +118,18 @@ float truncate(float x) {
   }
 }
 
-void calculateIK(int i){
+void calculateIK(struct Point point){
     // Wire length for given coordinates
-    l1 = sqrt(pow(sqrt(pow(boxLength - (X[i] + effectorLength/2.0), 2.0) + pow(boxWidth - (Y[i] + effectorWidth/2.0), 2.0)) - vertexRadius, 2.0) + pow(Z[i], 2.0));
-    l2 = sqrt(pow(sqrt(pow(X[i] - effectorLength/2.0, 2.0)               + pow(boxWidth - (Y[i] + effectorWidth/2.0), 2.0)) - vertexRadius, 2.0) + pow(Z[i], 2.0));
-    l3 = sqrt(pow(sqrt(pow(X[i] - effectorLength/2.0, 2.0)               + pow(Y[i] - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0)              + pow(Z[i], 2.0));
-    l4 = sqrt(pow(sqrt(pow(boxLength - (X[i] + effectorLength/2.0), 2.0) + pow(Y[i] - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0)              + pow(Z[i], 2.0));
+    l1 = sqrt(pow(sqrt(pow(boxLength - (point.x + effectorLength/2.0), 2.0) + pow(boxWidth - (point.y + effectorWidth/2.0), 2.0)) - vertexRadius, 2.0) + pow(point.z, 2.0));
+    l2 = sqrt(pow(sqrt(pow(point.x - effectorLength/2.0, 2.0)               + pow(boxWidth - (point.y + effectorWidth/2.0), 2.0)) - vertexRadius, 2.0) + pow(point.z, 2.0));
+    l3 = sqrt(pow(sqrt(pow(point.x - effectorLength/2.0, 2.0)               + pow(point.y - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0)              + pow(point.z, 2.0));
+    l4 = sqrt(pow(sqrt(pow(boxLength - (point.x + effectorLength/2.0), 2.0) + pow(point.y - effectorWidth/2.0, 2.0)) - vertexRadius, 2.0)              + pow(point.z, 2.0));
 
     // Step size needed for the new wire length
-    step1 += -(int) truncate((l1 - l1Prev)/stepAmount);
-    step2 += -(int) truncate((l2 - l2Prev)/stepAmount);
-    step3 += -(int) truncate((l3 - l3Prev)/stepAmount);
-    step4 += -(int) truncate((l4 - l4Prev)/stepAmount);
+    step1 = -(int) truncate((l1 - l1Prev)/stepAmount);
+    step2 = -(int) truncate((l2 - l2Prev)/stepAmount);
+    step3 = -(int) truncate((l3 - l3Prev)/stepAmount);
+    step4 = -(int) truncate((l4 - l4Prev)/stepAmount);
 
 
 
--- CDPR/99_main.ino
+++ CDPR/99_main.ino
@@ -5,18 +5,18 @@ String sData, data[10];
 bool isModified = false;
 
 void setup() {
-    
-//  initWiFi();
-  initXYZ();
+  initFirstPoint();
+  // initWiFi();
+  initCirclePath(150.0); // Radius
   initStepper();
   Serial.begin(9600);
-//  handleGetRoot();
+  // handleGetRoot();
+  initLoadCell();
 
-
-//  server.addHandler(&events);
+  // server.addHandler(&events);
 
   // Start server
-//  server.begin();
+  // server.begin();
 }
 
 
@@ -24,13 +24,65 @@ void loop() {
   while (Serial.available()) {
     programStart = Serial.read();
   }
-  if (programStart == '1') { 
-    // Inverse kinematics
-    for (int i = 0; i < n; i++) {
-      calculateIK(i);
-  
-      // Run the stepper
+  switch(programStart){
+    case '0':
+    {
+      Point point = {boxLength/2.0, boxWidth/2.0, zBias};
+      calculateIK(point);
       gerakStepper(step1, step2, step3, step4);
+      currentPosition[0] = boxLength/2.0;
+      currentPosition[1] = boxWidth/2.0;
+      currentPosition[2] = zBias;
+      break;
     }
+    case '1':
+    {   
+      int i = 0;
+      while(!Serial.available()){
+        calculateIK(points[i]);
+        currentPosition[0] = points[i].x;
+        currentPosition[1] = points[i].y;
+        currentPosition[2] = points[i].z;
+        gerakStepper(step1, step2, step3, step4);
+        i++;
+        if(i == n){
+          i = 0;
+        }
+        // Serial.println("x: " + String(currentPosition[0]) + " y: " + String(currentPosition[1]) + " z: " + currentPosition[2]);
+        readLoadCell();
+      }
+
+      break;
+    }
+    case '2':
+    {
+      Serial.print("x: y: ");
+      while(!Serial.available()){}
+      int x = Serial.parseInt();
+      int y = Serial.parseInt();
+      Serial.read();
+      Point point = {x,y,zBias};
+      calculateIK(point);
+      Serial.println(String(x) + " " + String(y));
+      gerakStepper(step1, step2, step3, step4);
+      programStart = -999;
+
+      currentPosition[0] = x;
+      currentPosition[1] = y;
+      currentPosition[2] = zBias;
+      break;
+    }
+
+    default:{
+      break;
+    }
+
   }
+ 
+  // Serial.println("x: " + String(currentPosition[0]) + " y: " + String(currentPosition[1]) + " z: " + currentPosition[2]);
+  readLoadCell();
 }
+
+
+
+
